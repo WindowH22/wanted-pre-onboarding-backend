b34f4ca52f299f10d54f35e2dc7b868ba01a13b7
WindowH22 <dlckdgml1235@naver.com>
2023-08-06 23:52:47 +0900

    feat: Add AuditingFields 재사용성을 위해 AuditingFields구현 Article 도메인에 상속 Article 도메인 @Builder 선언

--- src/main/java/com/windowh22/wantedpreonboardingbackend/domain/Article.java
+++ src/main/java/com/windowh22/wantedpreonboardingbackend/domain/Article.java
@@ -1,16 +1,16 @@
 package com.windowh22.wantedpreonboardingbackend.domain;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Setter;
-import lombok.ToString;
 
-import java.util.LinkedHashSet;
-
+@Builder
 @Entity
-public class Article {
+public class Article extends AuditingFields {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //mysql과 같은방식 ->AUTO로 설정 시 에러 남
-    private Long ArticleId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //mysql과 같은방식 ->AUTO로 설정 시 에러 남
+    private Long articleId;
 
     // setter를 따로 주는 이유는 자동으로 주는 값에 대해 인위적인 변경을 막기위함
     @Setter
@@ -20,10 +20,12 @@ public class Article {
     @JoinColumn(name = "userId")
     @ManyToOne(optional = false)
     private User user; // 유저 정보 (ID)
-    @Setter @Column(nullable = false, length = 10000)
+    @Setter
+    @Column(nullable = false, length = 10000)
     private String content; // 내용
 
-    protected Article(){}
+    protected Article() {
+    }
 
     private Article(User user, String title, String content) {
         this.user = user;
