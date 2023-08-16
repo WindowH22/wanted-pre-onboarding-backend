5c87bb0036cf0b7e246d0ca79dd88c3ae53968c1
WindowH22 <dlckdgml1235@naver.com>
2023-08-16 13:18:34 +0900

    feat: Add findByTitle #1

--- src/main/java/com/windowh22/wantedpreonboardingbackend/repository/ArticleRepository.java
+++ src/main/java/com/windowh22/wantedpreonboardingbackend/repository/ArticleRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
     Optional<Article> findByIdAndUser(Long id, User user);
+
+    Optional<Article> findByTitle(String title);
 }
